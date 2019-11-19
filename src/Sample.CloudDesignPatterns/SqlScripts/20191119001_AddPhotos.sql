86f301ee1d2f0653dc01072bef472d76f62a52d6
Burgyn <mino.martiniak@gmail.com>
2019-11-19 13:01:48 +0100

    fixe script

--- src/Sample.CloudDesignPatterns/SqlScripts/20191119001_AddPhotos.sql
+++ src/Sample.CloudDesignPatterns/SqlScripts/20191119001_AddPhotos.sql
@@ -2,8 +2,8 @@
     [Id] [int] NOT NULL,
     [Description] [nvarchar](255),
 	[Name] [nvarchar](100),
-	[Image] [varbinary] (maxlength),
-	[Thumbnail] [varbinary] (maxlength),
+	[Image] [varbinary],
+	[Thumbnail] [varbinary]
 CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED ([Id] ASC)
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
