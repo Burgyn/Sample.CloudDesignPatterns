7b38e0a1da075463c541a201edf19136210f2dc3
Burgyn <mino.martiniak@gmail.com>
2019-11-25 14:14:44 +0100

    Testing

--- infrastructure/Parse-ARMOutput.ps1
+++ infrastructure/Parse-ARMOutput.ps1
@@ -15,7 +15,3 @@ Write-Host " Krok 2"
 Write-Host "##vso[task.setvariable variable=AzureServiceBus.ConnectionString]$json.NamespaceConnectionString.value"
 
 Write-Host " Krok 3"
-
-Write-Host "Connection string je: $(AzureServiceBus.ConnectionString)"
-
-Write-Host " Krok 4"
