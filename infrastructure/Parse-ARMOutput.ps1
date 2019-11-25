bd842e4a91a754f0a6b49149b303f235bb167623
Burgyn <mino.martiniak@gmail.com>
2019-11-25 14:04:25 +0100

    Testing

--- infrastructure/Parse-ARMOutput.ps1
+++ infrastructure/Parse-ARMOutput.ps1
@@ -6,11 +6,16 @@ param (
 $json = $ARMOutput | convertfrom-json
 #endregion
 
-Write-Host "Krok 1 "
+Write-Host " Krok 1"
+
 Write-Output -InputObject ('Connection string {0} ' -f $json.NamespaceConnectionString.value)
 
-Write-Host "Krok 2 "
+Write-Host " Krok 2"
 
 Write-Host "##vso[task.setvariable variable=AzureServiceBus.ConnectionString]$json.NamespaceConnectionString.value"
 
-Write-Host "Krok 3 "
+Write-Host " Krok 3"
+
+Write-Host "Connection string je: $(AzureServiceBus.ConnectionString)"
+
+Write-Host " Krok 4"
